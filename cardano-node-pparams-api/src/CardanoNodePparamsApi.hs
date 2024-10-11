c1203ed7de4d851c24cec1acc8623c73ab261d54
John Lotoski <john.lotoski@iohk.io>
2024-10-10 19:55:43 -0500

    wip: switch to executeLocalStateQueryExpr for tip endpoint

--- cardano-node-pparams-api/src/CardanoNodePparamsApi.hs
+++ cardano-node-pparams-api/src/CardanoNodePparamsApi.hs
@@ -18,35 +18,53 @@ import Cardano.Api (
   ChainTip (..),
   ConsensusModeParams (CardanoModeParams),
   ConwayEra,
-  EpochNo,
+  EpochNo (..),
   EpochSlots (EpochSlots),
+  EraHistory (..),
   ExceptT (..),
   File (File),
   FromJSON,
-  Hash,
+  Hash (..),
   LocalNodeConnectInfo (LocalNodeConnectInfo),
+  MonadIO,
+  QueryConvenienceError (..),
   QueryInEra (QueryInShelleyBasedEra),
-  QueryInMode (QueryInEra),
+  QueryInMode (QueryInEra, QuerySystemStart),
   QueryInShelleyBasedEra (QueryEpoch, QueryProtocolParameters),
   ShelleyBasedEra (..),
   SlotNo,
+  SlotsInEpoch (..),
+  SlotsToEpochEnd (..),
+  SystemStart (..),
   ToJSON,
   docToLazyText,
+  except,
   executeLocalStateQueryExpr,
+  executeQueryAnyMode,
+  firstExceptT,
+  forEraInEon,
   getLocalChainTip,
+  getProgress,
+  hoistEither,
+  hoistMaybe,
+  hushM,
   left,
   lift,
   liftIO,
   makeChainTip,
   onLeft,
+  onNothing,
   queryChainBlockNo,
   queryChainPoint,
   queryCurrentEra,
   queryEraHistory,
+  queryExpr,
   queryNodeLocalState,
   querySystemStart,
   runExceptT,
+  slotToEpoch,
   throwE,
+  withExceptT,
  )
 import Cardano.Api.IPC (AcquiringFailure (..))
 import Cardano.Api.Ledger (
@@ -60,17 +78,38 @@ import Cardano.Api.Shelley (
   determineEra,
  )
 import Cardano.CLI.EraBased.Run.Query (percentage)
-import Cardano.CLI.Types.Errors.QueryCmdError (QueryCmdError (..), renderQueryCmdError)
-import qualified Cardano.CLI.Types.Output as O (QueryTipLocalState (..))
+import Cardano.CLI.Types.Errors.CmdError (CmdError (..))
+import Cardano.CLI.Types.Errors.QueryCmdError (
+  QueryCmdError (..),
+  renderQueryCmdError,
+ )
+import qualified Cardano.CLI.Types.Output as O (
+  QueryTipLocalState (..),
+  QueryTipLocalStateOutput (..),
+ )
+import Cardano.Crypto.Hash (Blake2b_256, digest, hashFromBytes, hashWith)
+import Control.Exception (Exception, throwIO)
+import Control.Monad (forM, join)
+import Control.Monad.Trans.Except.Extra (newExceptT)
 import Data.Aeson (encode)
-import Data.Bifunctor (first)
+import Data.Bifunctor (bimap, first)
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
+import qualified Data.ByteString.Char8 as BS
 import Data.ByteString.Lazy.UTF8 as BLU
+import Data.Either (fromRight)
 import Data.Function ((&))
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Proxy
 import Data.String (fromString)
+import Data.Text (Text)
+import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy.IO as LT
+import Data.Time (getCurrentTime)
+import Data.Time.Clock (secondsToNominalDiffTime)
 import Data.Typeable (Typeable, cast, typeOf, typeRep)
-import Data.Word (Word32)
+import Data.Word (Word32, Word64)
+import Debug.Trace (trace)
 import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp (
   HostPreference,
@@ -80,7 +119,12 @@ import Network.Wai.Handler.Warp (
   setPort,
   setTimeout,
  )
+import Ouroboros.Consensus.BlockchainTime.WallClock.Types (
+  RelativeTime (..),
+  toRelativeTime,
+ )
 import Ouroboros.Consensus.HardFork.Combinator.AcrossEras (EraMismatch)
+import Ouroboros.Consensus.HardFork.History (summaryWithExactly)
 import Ouroboros.Consensus.Shelley.Eras (StandardConway)
 import Servant (
   Get,
@@ -143,11 +187,12 @@ fetchPparamsHandler localNodeConn = do
 fetchTipHandler :: LocalNodeConnectInfo -> Handler (Headers '[Header "Cache-Control" String] Tip)
 fetchTipHandler localNodeConn = do
   tip <- liftIO $ queryTip localNodeConn
+  return $ addHeader cacheHeader tip
 
-  case tip of
-    Left err -> Handler $ throwE $ err500 {errBody = BLU.fromString $ show err}
-    Right (Left err) -> Handler $ throwE $ err500 {errBody = BLU.fromString $ show err}
-    Right (Right result) -> return $ addHeader cacheHeader result
+-- case tip of
+--   Left err -> Handler $ throwE $ err500 {errBody = BLU.fromString $ show err}
+--   Right (Left err) -> Handler $ throwE $ err500 {errBody = BLU.fromString $ show err}
+--   Right (Right result) -> return $ addHeader cacheHeader result
 
 -- TODO: make the sbe dynamic
 queryEpoch ::
@@ -172,49 +217,100 @@ queryPparams localNodeConn = do
         QueryInEra $
           QueryInShelleyBasedEra ShelleyBasedEraConway QueryProtocolParameters
 
--- TODO: improve the separate queries into one and add slotInEpoch, slotsToEpochEnd, syncProgress
-queryTip :: LocalNodeConnectInfo -> IO (Either AcquiringFailure (Either EraMismatch Tip))
+queryTip :: LocalNodeConnectInfo -> IO Tip
 queryTip localNodeConn = do
-  -- eLocalState <- ExceptT $
-  --   -- eLocalState <-
-  --   fmap sequence $
-  --     executeLocalStateQueryExpr localNodeConn VolatileTip $
-  --       runExceptT $ do
-  --         era <- lift queryCurrentEra & onLeft (left . QueryCmdUnsupportedNtcVersion)
-  --         eraHistory <- lift queryEraHistory & onLeft (left . QueryCmdUnsupportedNtcVersion)
-  --         mChainBlockNo <- lift queryChainBlockNo & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
-  --         mChainPoint <- lift queryChainPoint & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
-  --         mSystemStart <- lift querySystemStart & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
+  Right eLocalState <- runExceptT $
+    ExceptT $
+      fmap sequence $
+        executeLocalStateQueryExpr localNodeConn VolatileTip $
+          runExceptT $ do
+            era <- lift queryCurrentEra & onLeft (left . QueryCmdUnsupportedNtcVersion)
+            eraHistory <- lift queryEraHistory & onLeft (left . QueryCmdUnsupportedNtcVersion)
+            mChainBlockNo <- lift queryChainBlockNo & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
+            mChainPoint <- lift queryChainPoint & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
+            mSystemStart <- lift querySystemStart & onLeft (left . QueryCmdUnsupportedNtcVersion) & fmap Just
 
-  --         return
-  --           O.QueryTipLocalState
-  --             { O.era = era
-  --             , O.eraHistory = eraHistory
-  --             , O.mSystemStart = mSystemStart
-  --             , O.mChainTip = makeChainTip <$> mChainBlockNo <*> mChainPoint
-  --             }
+            return
+              O.QueryTipLocalState
+                { O.era = era
+                , O.eraHistory = eraHistory
+                , O.mSystemStart = mSystemStart
+                , O.mChainTip = makeChainTip <$> mChainBlockNo <*> mChainPoint
+                }
 
-  -- mLocalState <- hushM (first QueryCmdAcquireFailure eLocalState) $ \e ->
-  --   liftIO . LT.hPutStrLn stderr $
-  --     docToLazyText $
-  --       "Warning: Local state unavailable: " <> renderQueryCmdError e
+  mLocalState <- hushM (first QueryCmdAcquireFailure eLocalState) $ \e ->
+    liftIO . LT.hPutStrLn stderr $
+      docToLazyText $
+        "Warning: Local state unavailable: " <> renderQueryCmdError e
 
-  -- chainTip <-
-  --   pure (mLocalState >>= O.mChainTip)
-  --     & onNothing (queryChainTipViaChainSync localNodeConnInfo)
+  Right chainTip <-
+    runExceptT $
+      pure (mLocalState >>= O.mChainTip)
+        -- The chain tip is unavailable via local state query because we are connecting with an older
+        -- node to client protocol so we use chain sync instead which necessitates another connection.
+        -- At some point when we can stop supporting the older node to client protocols, this fallback
+        -- can be removed.
+        & onNothing (lift $ queryChainTipViaChainSync localNodeConn)
 
-  era' <- runExceptT $ determineEra localNodeConn
-  epoch' <- queryEpoch localNodeConn
-  chainTip' <- runExceptT $ getLocalChainTip localNodeConn
+  let (tipSlotNo :: SlotNo, hash :: (Hash BlockHeader), blockNo :: BlockNo) = case chainTip of
+        -- How to hash sample data and cast to (Hash BlockHeader?)
+        -- ChainTipAtGenesis -> (0, "0", 0)
+        ChainTip slotNo hash blockNo -> (slotNo, hash, blockNo)
 
-  case (era', epoch', chainTip') of
-    (Left err, _, _) -> return $ Left err
-    (_, Left err, _) -> return $ Left err
-    (_, _, Left err) -> return $ Left err
-    (_, Right (Left eraMismatch), _) -> return $ Right $ Left eraMismatch
-    (Right era, Right (Right epoch), Right chainTip) -> case chainTip of
-      ChainTipAtGenesis -> return $ Left AFPointTooOld
-      ChainTip slot hash block -> return $ Right $ Right $ Tip block epoch era hash slot
+  Just localStateOutput <- forM mLocalState $ \localState -> do
+    case slotToEpoch tipSlotNo (O.eraHistory localState) of
+      Left e -> do
+        liftIO . LT.hPutStrLn stderr $
+          docToLazyText $
+            "Warning: Epoch unavailable: " <> renderQueryCmdError (QueryCmdPastHorizon e)
+        return $
+          O.QueryTipLocalStateOutput
+            { O.localStateChainTip = chainTip
+            , O.mEra = Nothing
+            , O.mEpoch = Nothing
+            , O.mSyncProgress = Nothing
+            , O.mSlotInEpoch = Nothing
+            , O.mSlotsToEpochEnd = Nothing
+            }
+      Right (epochNo, SlotsInEpoch slotsInEpoch, SlotsToEpochEnd slotsToEpochEnd) -> do
+        syncProgressResult <- runExceptT $ do
+          systemStart <-
+            fmap getSystemStart (O.mSystemStart localState) & hoistMaybe QueryCmdSystemStartUnavailable
+          nowSeconds <- toRelativeTime (SystemStart systemStart) <$> liftIO getCurrentTime
+          tipTimeResult <-
+            getProgress tipSlotNo (O.eraHistory localState) & bimap QueryCmdPastHorizon fst & except
+
+          let tolerance = RelativeTime (secondsToNominalDiffTime 600)
+
+          return $ percentage tolerance tipTimeResult nowSeconds
+
+        mSyncProgress <- hushM syncProgressResult $ \e -> do
+          liftIO . LT.hPutStrLn stderr $
+            docToLazyText $
+              "Warning: Sync progress unavailable: " <> renderQueryCmdError e
+
+        -- Extend past this return structure to include system start utc
+        return $
+          O.QueryTipLocalStateOutput
+            { O.localStateChainTip = chainTip
+            , O.mEra = Just (O.era localState)
+            , O.mEpoch = Just epochNo
+            , O.mSlotInEpoch = Just slotsInEpoch
+            , O.mSlotsToEpochEnd = Just slotsToEpochEnd
+            , O.mSyncProgress = mSyncProgress
+            }
+
+  -- Implement proper exception propagation and handling
+  return $
+    Tip
+      blockNo
+      (fromMaybe (EpochNo 0) $ O.mEpoch localStateOutput)
+      (fromMaybe (AnyCardanoEra ByronEra) $ O.mEra localStateOutput)
+      hash
+      tipSlotNo
+      (fromMaybe (0 :: Word64) $ O.mSlotInEpoch localStateOutput)
+      (fromMaybe (0 :: Word64) $ O.mSlotsToEpochEnd localStateOutput)
+      (fromMaybe ("0.00" :: Text) $ O.mSyncProgress localStateOutput)
 
 data Tip = Tip
   { block :: BlockNo
@@ -222,6 +318,9 @@ data Tip = Tip
   , era :: AnyCardanoEra
   , hash :: Hash BlockHeader
   , slot :: SlotNo
+  , slotInEpoch :: Word64
+  , slotToEpochEnd :: Word64
+  , syncProgress :: Text
   }
   deriving (Show, Generic)
 
@@ -268,6 +367,17 @@ validateShelleyEraOrExit era = case era of
 -- printEither (Left a) = putStrLn ("Left: " ++ show a)
 -- printEither (Right b) = putStrLn ("Right: " ++ show b)
 
+-- | Query the chain tip via the chain sync protocol.
+--
+-- This is a fallback query to support older versions of node to client protocol.
+queryChainTipViaChainSync :: MonadIO m => LocalNodeConnectInfo -> m ChainTip
+queryChainTipViaChainSync localNodeConnInfo = do
+  liftIO . T.hPutStrLn stderr $
+    "Warning: Local header state query unavailable. Falling back to chain sync query"
+  liftIO $ getLocalChainTip localNodeConnInfo
+
+instance Exception QueryCmdError
+
 main :: IO ()
 main = do
   bindHost <- readEnvVarOrExit @String "WARP_BIND_HOST"
@@ -283,10 +393,17 @@ main = do
             setPort bindPort defaultSettings
 
   era <- runExceptT $ determineEra localNodeConn
+
   validateShelleyEraOrExit era
 
   runSettings settings $ serve api $ apiServer localNodeConn
 
+-- Alternative query which runs into same constraint issue:
+-- Right (AnyCardanoEra era) <- runExceptT $ determineEra localNodeConn
+-- sbe <- forEraInEon @ShelleyBasedEra era (throwIO QueryCmdByronEra) pure
+-- let qInMode = QueryInEra $ QueryInShelleyBasedEra sbe QueryProtocolParameters
+-- pp <- runExceptT $ executeQueryAnyMode localNodeConn qInMode
+--
 -- For debug outside of the api server:
 -- pparams <- queryPparams localNodeConn
 -- case pparams of
